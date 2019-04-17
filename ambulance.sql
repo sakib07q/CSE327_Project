0a87453e3d4a822f2b1a7ea7ad206dc51031197c
sakib <sakib.swroar2018@northsouth.edu>
2019-04-17 12:50:04 +0600

    sql updated

--- ambulance.sql
+++ ambulance.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 16, 2019 at 06:33 PM
+-- Generation Time: Apr 17, 2019 at 08:48 AM
 -- Server version: 10.1.36-MariaDB
 -- PHP Version: 7.2.10
 
@@ -42,25 +42,26 @@ CREATE TABLE `ambulance` (
 
 INSERT INTO `ambulance` (`id`, `Name`, `contact`, `charges`, `images`) VALUES
 (1, 'Apollo Hospitals Dhaka\r\n', '+8801714-090000', '500', 'images/ApolloHospitalAmbu_zps95879c9e.jpg'),
-(2, 'Apanjon Ambulance Service\r\n', '9125420', '500', 'images/'),
-(3, 'Alif Ambulance\r\n', '8117576', '500', 'images/'),
-(4, 'Ad-Din Hospital\r\n', '+880-2- 9362929\r\n', '500', 'images/'),
-(5, 'Anju-Man-E-Mufidul Islam\r\n', '+880-2-9336611', '500', 'images/'),
-(6, 'Bangabondhu Sheikh Mujib Medical University (BSMMU) (PG Hospital)', '+880-2-8614001-5', '500', 'images/'),
-(7, 'Bangladesh Medical College Hospital\r\n', '+880-2-9118202', '500', 'images/'),
-(8, 'CMH (Dhaka)\r\n', '+880-2-8822779\r\n\r\n', '500', 'images/'),
-(9, 'Dhaka Eye Hospital\r\n', '+880-2-8014476', '500', 'images/'),
-(10, 'Dhaka Medical College Hospital', '+880-2-9669340\r\n\r\n', '500', 'images/'),
-(11, 'Fire Brigade Ambulance Service\r\n', '+880-2-9553333-7', '500', 'images/'),
-(12, 'Heart Hospital', '+880-2-9803302', '500', 'images/'),
-(13, 'Life Line Healthcare Limited\r\n', '+8801199-899919', '500', 'images/'),
-(14, 'Green Ambulance Service', '8612412', '500', 'images/'),
-(15, 'Rafa Ambulance Service\r\n', '9110663', '500', 'images/'),
-(16, 'BIRDEM\r\n', '9661551-60', '500', 'images/'),
-(17, 'Dhaka City Corporation (Mirpur Control Room)\r\n', '9004734', '500', 'images/'),
-(18, 'Shahid Suhrawardi Hospital, Ambulance Service\r\n', '9130800', '500', 'images/'),
-(19, 'Shishu Hospital, Ambulance Service\r\n', '8116061-2', '500', 'images/'),
-(20, 'Square Hospital\r\n', '+880-2-8144466', '500', 'images/');
+(2, 'Apanjon Ambulance Service\r\n', '9125420', '500', 'images/download.jpg'),
+(3, 'Alif Ambulance\r\n', '8117576', '500', 'images/Alif-Ambulance-Service-400x280.jpg'),
+(4, 'Ad-Din Hospital\r\n', '+880-2- 9362929\r\n', '350', 'images/ambulance.jpg'),
+(5, 'Anju-Man-E-Mufidul Islam\r\n', '+880-2-9336611', '500', 'images/15_photo_47406.jpg'),
+(6, 'Bangabondhu Sheikh Mujib Medical University (BSMMU) (PG Hospital)', '+880-2-8614001-5', '500', 'images/bsmmu.png'),
+(7, 'Bangladesh Medical College Hospital\r\n', '+880-2-9118202', '500', 'images/bangmed.jpg'),
+(8, 'CMH (Dhaka)\r\n', '+880-2-8822779\r\n\r\n', '500', 'images/cmh.png'),
+(9, 'Dhaka Eye Hospital\r\n', '+880-2-8014476', '500', 'images/eye.jpg'),
+(10, 'Dhaka Medical College Hospital', '+880-2-9669340\r\n\r\n', '500', 'images/dhamek.jpg'),
+(11, 'Fire Brigade Ambulance Service\r\n', '+880-2-9553333-7', '500', 'images/fire_service.jpg'),
+(12, 'Heart Hospital', '+880-2-9803302', '500', 'images/heart.png'),
+(13, 'Life Line Healthcare Limited\r\n', '+8801199-899919', '350', 'images/lifeline.jpg'),
+(14, 'Green Ambulance Service', '8612412', '500', 'images/green.jpg'),
+(15, 'Rafa Ambulance Service\r\n', '9110663', '500', 'images/rafa.jpg'),
+(16, 'BIRDEM\r\n', '9661551-60', '500', 'images/BIRDEM-General-Hospital-3rd-slide.jpg'),
+(17, 'Dhaka City Corporation (Mirpur Control Room)\r\n', '9004734', '500', 'images/dcc.jpg'),
+(18, 'Shahid Suhrawardi Hospital, Ambulance Service\r\n', '9130800', '500', 'images/sohra.jpg'),
+(19, 'Shishu Hospital, Ambulance Service\r\n', '8116061-2', '500', 'images/Dhaka Shishu (Child) Hospital.jpg'),
+(20, 'Square Hospital\r\n', '+880-2-8144466', '500', 'images/square.jpg'),
+(21, 'labaid', '0171330088', '400', 'images/labaid.jpg');
 
 --
 -- Indexes for dumped tables
@@ -80,7 +81,7 @@ ALTER TABLE `ambulance`
 -- AUTO_INCREMENT for table `ambulance`
 --
 ALTER TABLE `ambulance`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
